413be9c86ec9d13321968760f8c5bf07ffded086
Strutsiboi <melker.johansson@elev.ga.lbs.se>
2022-01-13 15:31:53 +0100

    Ai och Pendel

--- Assets/Scripts/Melkers Scripts/EnemyFollowRange.cs
+++ Assets/Scripts/Melkers Scripts/EnemyFollowRange.cs
@@ -8,6 +8,13 @@ public class EnemyFollowRange : MonoBehaviour
     public float lineOfSite;
     private Transform player;
 
+    public float shootingRange;
+    public float fireRate = 1f;
+    private float nextFireTime;
+
+    public GameObject bullet;
+    public GameObject bulletParent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +25,21 @@ public class EnemyFollowRange : MonoBehaviour
     void Update()
     {
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
-        if (distanceFromPlayer < lineOfSite)
+        if (distanceFromPlayer < lineOfSite && distanceFromPlayer>shootingRange)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
-        }    
+        }
+        else if (distanceFromPlayer<shootingRange && nextFireTime <Time.time)
+        {
+            Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
+            nextFireTime = Time.time + fireRate;
+        }
     }
 
     public void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, lineOfSite);
+        Gizmos.DrawWireSphere(transform.position, shootingRange);
     }
 }
