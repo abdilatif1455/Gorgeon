6ffdf6ed8a34c14f62e29f8b21c8ea6ecbeef13e
HerotheHerotheGodofDeath <79506131+HerotheHerotheGodofDeath@users.noreply.github.com>
2022-01-13 13:59:47 +0100

    Är Halvklar med roomtransition.

---
+++ Assets/Scripts/Mattias Scripts/Transition.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public GameObject camPoint;
+    public GameObject currentRoom;
+    public GameObject formerRoom;
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Camera.main.transform.position = camPoint.transform.position;
+            currentRoom.SetActive(true);
+            //player.transform.position = Vector2(currentlocation + 1, 0);
+            formerRoom.SetActive(false);
+        }
+    }
+}
