8375006616d2ab014807fa62d9d395d4a4cd6d13
HerotheHerotheGodofDeath <79506131+HerotheHerotheGodofDeath@users.noreply.github.com>
2022-01-14 13:50:57 +0100

    Objection.

---
+++ Assets/Scripts/Mattias Scripts/Boss.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public float attackcountdown;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
